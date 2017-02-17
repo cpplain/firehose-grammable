85bc57b9298320ed3b3fed33ac7c3ff9d5436a0f
Christopher Plain <christopher@jetcitycoder.com>
2017-02-17 05:18:53 +0000

    Allow users to delete grams

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -95,4 +95,19 @@ RSpec.describe GramsController, type: :controller do
       expect(gram.message).to eq 'Initial value'
     end
   end
+
+  describe 'grams#destroy action' do
+    it 'should allow a user to destroy grams' do
+      gram = FactoryGirl.create(:gram)
+      delete :destroy, params: { id: gram.id }
+      expect(response).to redirect_to root_path
+      gram = Gram.find_by_id(gram.id)
+      expect(gram).to eq nil
+    end
+
+    it 'should return a 404 error if the gram cannot be found' do
+      delete :destroy, params: { id: 'CATDOG' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
 end
